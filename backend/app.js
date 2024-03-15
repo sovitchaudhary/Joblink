90b4caaf903b458fd608095f805f930dd18f1123
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-15 13:49:46 +0530

    Set the cors, cookieParser and

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,25 @@
 import express from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
 
 const app = express();
 dotenv.config({path: "./config/config.env"});
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/temp/",
+}))
 
 export default app;
