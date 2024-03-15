a70230049225f1aaff0f5aa731cdbf5913c38578
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-16 00:57:11 +0530

    Added error functionality

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,14 @@
+import { catchAsyncError } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userSchema.js";
+
+export const isAuthorized = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("User not authorized", 400));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  req.user = await User.findById(decoded.id);
+});
