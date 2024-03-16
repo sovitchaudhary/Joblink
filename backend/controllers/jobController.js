0d3887bc5bb3921f474ae9aa84fdc6df2c5d2ccd
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-16 13:30:49 +0530

    Added Delete and update Job funcitonality

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -58,3 +58,53 @@ export const postJob = catchAsyncError(async (req, res, next) => {
     job,
   });
 });
+
+export const getMyJobs = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(new ErrorHandler("Job Seeker not allowed to access this resource.", 400));
+  }
+  const myJobs = await Job.find({ postedBy: req.user._id });
+  res.status(200).json({
+    success: true,
+    myJobs,
+  });
+});
+
+export const updateJob = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(new ErrorHandler("Job Seeker not allowed to access this resource.", 400));
+  }
+  const { id } = req.params;
+  let job = await Job.findById(id);
+  if (!job) {
+    return next(new ErrorHandler("OOPS! Job not found.", 404));
+  }
+  job = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Job Updated!",
+  });
+});
+
+export const deleteJob = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(new ErrorHandler("Job Seeker not allowed to access this resource.", 400));
+  }
+  const { id } = req.params;
+  const job = await Job.findById(id);
+  if (!job) {
+    return next(new ErrorHandler("OOPS! Job not found.", 404));
+  }
+  await job.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Job Deleted!",
+  });
+});
--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import {postJob, getAllJobs} from "../controllers/jobController.js";
+import { getAllJobs, postJob, getMyJobs, updateJob, deleteJob} from "../controllers/jobController.js";
 import { isAuthorized } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.get("/getall", getAllJobs);
 router.post("/post",isAuthorized, postJob);
+router.get("/getmyjobs", isAuthorized, getMyJobs);
+router.put("/update/:id", isAuthorized, updateJob);
+router.delete("/delete/:id", isAuthorized, deleteJob);
 
 export default router;
