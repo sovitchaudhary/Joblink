3f83badcc5f39b501d2be4c2e629fdff1e182914
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-16 20:38:03 +0530

    Added Application functionality

--- backend/routes/applicationRouter.js
+++ backend/routes/applicationRouter.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import { employerGetAllApplications, jobseekerDeleteApplication, jobseekerGetAllApplications, postApplication } from '../controllers/applicationController.js';
+import { isAuthorized } from '../middlewares/auth.js';
 
 const router = express.Router();
+
+router.post("/post", isAuthorized, postApplication);
+router.get("/employer/getall", isAuthorized, employerGetAllApplications);
+router.get("/jobseeker/getall", isAuthorized, jobseekerGetAllApplications);
+router.delete("/delete/:id", isAuthorized, jobseekerDeleteApplication
+);
+
 
 export default router;
