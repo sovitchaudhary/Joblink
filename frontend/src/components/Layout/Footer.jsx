81210f9876d8cbb985f0699f542a39d3b71d4aac
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-17 01:29:31 +0530

    Footer design

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 
 const Footer = () => {
-  return (
-    <>
 
-    </>
+  const { isAuthorized } = useContext(Context);
+
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By CodeWithSovit.</div>
+      <div>
+        <Link to={"https://www.facebook.com/"} target="_blank">
+          <FaFacebookF className="fb" />
+        </Link>
+        <Link to={"https://www.youtube.com/"} target="_blank">
+          <FaYoutube className="yt" />
+        </Link>
+        <Link to={"https://www.youtube.com/"} target="_blank">
+          <FaLinkedin className="ln" />
+        </Link>
+        <Link to={"https://www.instagram.com/"} target="_blank">
+          <RiInstagramFill className="Inst" />
+        </Link>
+      </div>
+    </footer>
   )
 }
 
