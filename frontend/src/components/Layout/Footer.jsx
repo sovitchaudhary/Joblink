30718e1be23cfcca4feb8a755eb8c9a11a873e75
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-17 01:24:37 +0530

    Revert "Footer design"
    This reverts commit 3a70d5db34f95ae15d47162f1932428ba1c27b23.

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -1,31 +1,10 @@
-import React, { useContext } from "react";
-import { Context } from "../../main";
-import { Link } from "react-router-dom";
-import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
+import React from 'react'
 
 const Footer = () => {
-
-  const { isAuthorized } = useContext(Context);
-
   return (
-    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
-      <div>&copy; All Rights Reserved By CodeWithSovit.</div>
-      <div>
-        <Link to={"https://www.facebook.com/profile.php?id=100030535123397"} target="_blank">
-          <FaFacebookF className="fb" />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
-          <FaYoutube className="yt" />
-        </Link>
-        <Link to={"https://www.youtube.com/@CodeWithZeeshu"} target="_blank">
-          <FaLinkedin className="ln" />
-        </Link>
-        <Link to={"https://www.instagram.com/z_4_zeeshuuu/"} target="_blank">
-          <RiInstagramFill className="Inst" />
-        </Link>
-      </div>
-    </footer>
+    <>
+
+    </>
   )
 }
 
