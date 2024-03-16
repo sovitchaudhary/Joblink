d08bbc705dc55047513bdff7177cc9758cdbaf86
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-17 00:23:15 +0530

    Design a nofound page

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <>
+    <section className="page notfound">
+      <div className="content">
+        <img src="/notfound.png" alt="notfound" />
+        <Link to={"/"}>Return To Home</Link>
+      </div>
+    </section>
+  );
+};
 
-    </>
-  )
-}
-
-export default NotFound
+export default NotFound;
