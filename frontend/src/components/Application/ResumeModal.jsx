7f8942d9421a460bea4d78916390f3de09af6851
sovitchaudhary <sovitchaudhary45@gmail.com>
2024-03-17 16:23:27 +0530

    Design MyApplication and ResumeModel components

--- frontend/src/components/Application/ResumeModal.jsx
+++ frontend/src/components/Application/ResumeModal.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import PropTypes from "prop-types";
 
-const ResumeModal = () => {
+const ResumeModal = ({ imageUrl, onClose }) => {
   return (
-    <>
+    <div className="resume-modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <img src={imageUrl} alt="resume" />
+      </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
+// Define prop types for the ResumeModal component
+ResumeModal.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
-export default ResumeModal
+export default ResumeModal;
